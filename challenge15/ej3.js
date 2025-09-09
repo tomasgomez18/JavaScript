3e4f2b2c5bbec54587729fc899dfe6dd3518b2bb
tomas <tomasgomezzi36@gmail.com>
2025-09-09 17:45:18 -0300

    add exercises

---
+++ challenge15/ej3.js
@@ -0,0 +1,34 @@
+
+
+const tareas =
+[ { id: 1, descripcion: "Hacer ejercicio", completada: true, prioridad: "alta" }, 
+{ id: 2, descripcion: "Estudiar JavaScript", completada: false, prioridad: "alta" }, 
+{ id: 3, descripcion: "Comprar víveres", completada: false, prioridad: "media" }, 
+{ id: 4, descripcion: "Llamar al médico", completada: true, prioridad: "baja" }, 
+{ id: 5, descripcion: "Limpiar la casa", completada: false, prioridad: "media" } ]
+
+
+//Mostrar las tareas que están pendientes (no completadas)
+
+
+const tarea = tareas.filter(items => items.completada ===false)
+console.log(tarea);
+
+
+//Crear un array solo con las descripciones de las tareas
+
+const descripcionTareas = tareas.map(tarea => tarea.descripcion);
+console.log(descripcionTareas);
+
+
+//Filtrar las tareas de alta prioridad que no están completadas
+
+const tareasAltaPrioridad = tareas.filter (tarea => tarea.prioridad === 'alta')
+console.log(tareasAltaPrioridad);
+
+
+//Encontrar la tarea que contiene "JavaScript" en su descripción
+
+const palabra = tareas.find(palabra => palabra.descripcion === 'JavaScript')
+console.log(palabra);
+
