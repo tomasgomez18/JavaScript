ea4899b08f4faa0c4f237812f4f97220fbd777d3
tomas <tomasgomezzi36@gmail.com>
2025-09-09 17:51:02 -0300

    Correction of exercise 3

--- challenge15/ej3.js
+++ challenge15/ej3.js
@@ -29,6 +29,6 @@ console.log(tareasAltaPrioridad);
 
 //Encontrar la tarea que contiene "JavaScript" en su descripción
 
-const palabra = tareas.find(palabra => palabra.descripcion === 'JavaScript')
-console.log(palabra);
+const palabra = tareas.filter(tarea=> tarea.descripcion.includes("JavaScript"))
+console.log(palabra);   
 
