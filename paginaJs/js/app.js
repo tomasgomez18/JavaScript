5f3f76b2ba7f84d57c2e97a547139d22fa477069
tomas <tomasgomezzi36@gmail.com>
2025-09-09 09:45:20 -0300

    Add exercises

---
+++ paginaJs/js/app.js
@@ -0,0 +1,6 @@
+const cambiarTitulo = () => {
+    //Generamos una funcion y verificamos por consola//
+    console.log("Desde la funcion cambiar titulo");
+    const  titulo  = document.getElementById("titulo-modificable");
+    console.log(titulo);
+    }
