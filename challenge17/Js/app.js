2646b9e516b7c81ca2311fdc3f4265edddb3bac6
tomas <tomasgomezzi36@gmail.com>
2025-11-07 18:13:02 -0300

    boton eliminar de la lista de tareas eliminado

--- challenge17/Js/app.js
+++ challenge17/Js/app.js
@@ -43,8 +43,19 @@ function currentTime(){
             texto.innerText = input.value;
             //inserto el elemento p adentro del div contenedor
             tareaNueva.appendChild(texto)
-            //inserto el div completo al contenedotr principal para que se vea visualmente en la pagina
             listaDeTareas.appendChild(tareaNueva);
+
+
+            let iconos = document.createElement('div');
+            iconos.classList.add('iconos');
+            let eliminar = document.createElement('i');
+            eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
+            eliminar.addEventListener('click', function() {
+                tareaNueva.remove(); 
+            });
+
+            iconos.appendChild(eliminar);
+            tareaNueva.appendChild(iconos);
         }
     }
     //llamo a la funcion en el boton para que al hacer click se ejecute la funcion
