44810185cf7dae7f0576be74ebc42d7c6d818a78
tomas <tomasgomezzi36@gmail.com>
2025-11-08 11:20:13 -0300

    boton eliminar terminado y funcionando

--- challenge17/Js/app.js
+++ challenge17/Js/app.js
@@ -48,7 +48,7 @@ function currentTime(){
 
             let iconos = document.createElement('div');
             iconos.classList.add('iconos');
-            let eliminar = document.createElement('i');
+           let eliminar = document.createElement('i');
             eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
             eliminar.addEventListener('click', function() {
                 tareaNueva.remove(); 
